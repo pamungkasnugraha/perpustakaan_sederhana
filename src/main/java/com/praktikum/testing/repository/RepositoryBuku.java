83ef3decab0762d86a25c1ffa84b6414d57ebc71
Pamungkasz <pamungkasnugraha9@gmail.com>
2025-10-01 15:21:59 +0700

    Add files via upload

---
+++ src/main/java/com/praktikum/testing/repository/RepositoryBuku.java
@@ -0,0 +1,15 @@
+package com.praktikum.testing.repository;
+
+import com.praktikum.testing.model.Buku;
+import java.util.List;
+import java.util.Optional;
+
+public interface RepositoryBuku {
+    boolean simpan(Buku buku);
+    Optional<Buku> cariByIsbn(String isbn);
+    List<Buku> cariByJudul(String judul);
+    List<Buku> cariByPengarang(String pengarang);
+    boolean hapus(String isbn);
+    boolean updateJumlahTersedia(String isbn, int jumlahTersediaBaru);
+    List<Buku> cariSemua();
+}
